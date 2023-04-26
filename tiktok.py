962ff12cafeef80adadf19531f000f18ec5aec4a
-∞WKS∞- <121689078+WKS410@users.noreply.github.com>
2023-04-25 23:53:12 -0500

    Add files via upload

--- tiktok.py
+++ tiktok.py
@@ -1,11 +1,12 @@
-import logging
-import coloredlogs
-import requests
-import subprocess
-import os
 import argparse
-from bs4 import BeautifulSoup
+import requests
+import signal
+import sys
+import os
+import coloredlogs
+import logging
 from tqdm import tqdm
+
 
 # Define service
 SERVICE = 'TIKTOK'
@@ -14,52 +15,83 @@ SERVICE = 'TIKTOK'
 LOG_FORMAT = "%(asctime)s - [{level[0]}] - {service} - %(message)s".format(level="levelname", service=SERVICE)
 LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 coloredlogs.install(level='INFO', fmt=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
+logger = logging.getLogger(SERVICE)
 
-# TikTok API Headers
-headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'es-ES,es;q=0.9',
-    'Connection': 'keep-alive',
-    'Content-Type': 'application/json; charset=UTF-8',
-    'Origin': 'https://www.tiktok.com',
-    'Referer': 'https://www.tiktok.com/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
-    'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-}
 
-class TikTokDownloader:
-    def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_url = f"https://tikwm.com/video/media/hdplay/{video_id}.mp4"
-
-    def download_video(self):
+def download_video(url, filename):
         # Start downloading video
-        logging.info("Downloading video...")
-        r = requests.head(self.video_url, allow_redirects=True)
-        total_size = int(r.headers.get('content-length', 0))
-        block_size = 1024
-        t=tqdm(total=total_size, unit='iB', unit_scale=True)
-        with requests.get(self.video_url, stream=True) as r:
-            r.raise_for_status()
-            with open(f"{self.video_id}_TikTok.mp4", 'wb') as f:
-                for chunk in r.iter_content(chunk_size=block_size):
-                    if chunk:
-                        f.write(chunk)
-                        t.update(len(chunk))
-        t.close()
-        logging.info("Video downloaded successfully!")
-        
+    logging.info("Downloading video...")
+    response = requests.get(url, stream=True)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-id", "--id", dest="id_tiktok", help="Url Tiktok", required=True)
+    # obtiene el tamaño del archivo a descargar en bytes
+    file_size = int(response.headers.get('Content-Length', 0))
+
+    # crea una instancia de tqdm
+    progress_bar = tqdm(total=file_size, unit='B', unit_scale=True, desc=f'Downloading {filename}')
+
+    with open(filename, 'wb') as file:
+        for data in response.iter_content(chunk_size=1024):
+            # escribe los datos del archivo en el disco
+            file.write(data)
+
+            # actualiza el indicador de progreso
+            progress_bar.update(len(data))
+
+            # verifica si se ha recibido una señal de interrupción
+            if stop_signal:
+                break
+
+    # cierra la instancia de tqdm
+    progress_bar.close()
+
+
+def sigint_handler(signal, frame):
+    global stop_signal
+    stop_signal = True
+
+
+if __name__ == '__main__':
+    # registra el manejador de señal SIGINT
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    # analiza los argumentos de la línea de comandos
+    parser = argparse.ArgumentParser(description='Download a TikTok video file using tqdm.')
+    parser.add_argument('-id', '--input-id', dest='input_id', help='ID of the video file to download.')
+    parser.add_argument('-i', '--input-url', dest='input_url', help='URL of the live video file to download.')
     args = parser.parse_args()
-    video_id = str(args.id_tiktok) 
 
-    downloader = TikTokDownloader(video_id)
-    downloader.download_video()
+    # indica que no se ha recibido una señal de interrupción
+    stop_signal = False
+
+    if args.input_id:
+        # descarga el video normal
+        url = f'https://tikwm.com/video/media/hdplay/{args.input_id}.mp4'
+        filename = f'{args.input_id}_TikTok.mp4'
+
+        try:
+            # descarga el archivo de video
+            download_video(url, filename)
+        except KeyboardInterrupt:
+            # interrupción por teclado
+            logger.info('Download interrupted.')
+            sys.exit(0)
+
+        logger.info('Download completed.')
+
+    elif args.input_url:
+        # descarga el live
+        url = args.input_url
+        filename = 'live_tiktok.mp4'
+
+        try:
+            # descarga el archivo de video
+            download_video(url, filename)
+        except KeyboardInterrupt:
+            # interrupción por teclado
+            logger.info('Download interrupted.')
+            sys.exit(0)
+
+        logger.info('Download completed.')
+
+    else:
+        logger.error('Please specify an ID or URL.')
